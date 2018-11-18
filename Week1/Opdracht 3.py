8fc4ab273fe310eef5e6583c5a4df9d87e9285c0
denizlektemur <31707186+denizlektemur@users.noreply.github.com>
2018-11-18 13:53:10 +0100

    Opdracht 3 verbetering

--- Week1/Opdracht 3.py
+++ Week1/Opdracht 3.py
@@ -1,18 +1,20 @@
-def make_prime(size):
-    prime_list = []
-    mark = size+1
+numbers_list = []
+prime_list = []
+mark_list = [False] * 999
 
-    for i in range(2,size):
-        prime_list.append(i)
+for i in range(2, 1001):
+    numbers_list.append(i)
 
-    for i in prime_list:
-        for j in prime_list[i:]:
-            if j%i == 0:
-                prime_list[prime_list.index(j)] = mark
+for j in numbers_list:
+    counter = 2
+    number = j * counter
+    while number < 1001:
+        mark_list[numbers_list.index(number)] = True
+        counter += 1
+        number = j * counter
 
-    while mark in prime_list:
-        prime_list.remove(mark)
+for k in range(len(mark_list)):
+    if mark_list[k] == False:
+        prime_list.append(numbers_list[k])
 
-    print(prime_list)
-
-make_prime(1000)
+print(prime_list)
