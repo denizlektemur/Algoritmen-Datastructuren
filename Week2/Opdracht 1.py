8419cc509661d6be013fe999856b58aa0230b84b
Deniz <deniz.lektemur@student.hu.nl>
2018-11-29 14:02:50 +0100

    Opdracht 1 verbetering

--- Week2/Opdracht 1.py
+++ Week2/Opdracht 1.py
@@ -1,14 +1,16 @@
 def machtv3(basisgetal,macht):
     assert macht > 0
-
+    vermenigvuldigingen = 0
     resultaat = 1
     while macht > 0:
+        vermenigvuldigingen += 1
         if macht%2 == 0:
             basisgetal *= basisgetal
             macht /= 2
         else:
-            resultaat *= a
+            resultaat *= basisgetal
             macht -= 1
-    return resultaat
+    return resultaat,vermenigvuldigingen
 
-print(machtv3(2,10))
+my_macht = machtv3(4,7)
+print( "4 ** 7 =", my_macht[0], "met", my_macht[1], "vermenigvuldigingen")
