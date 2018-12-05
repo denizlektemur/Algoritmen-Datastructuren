fca9152f65c4f8d9121d4b835391a2f40dd04c88
Deniz <deniz.lektemur@student.hu.nl>
2018-12-05 16:04:50 +0100

    trie update

--- Week3/Opdracht 4.py
+++ Week3/Opdracht 4.py
@@ -13,15 +13,112 @@ def makeWordsDict(text):
             word = word.strip("!")
             word = word.strip("?")
             word = word.strip(":")
-            if word.__hash__() in words_dict.keys():
-                words_dict[word] += 1
-            else:
-                words_dict[word.__hash__()] = word
-                words_dict[word] = 1
+            if word != "":
+                if word in words_dict:
+                    words_dict[word] += 1
+                else:
+                    words_dict[word] = 1
 
     return words_dict
 
+def dictToTxt(words_dict):
+    text = open("dictFreq.txt", "w")
+    for key in words_dict:
+        text.write(key + " " + str(words_dict[key]) + "\n")
+    text.close()
+
 my_text = open("TextFile.txt", "r")
+my_second_text = open("TextFile.txt", "r")
 my_dict = makeWordsDict(my_text)
+dictToTxt(my_dict)
 print("The word \"the\" occurs", my_dict["the"], "times")
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.value = 0
+
+    def addWord(self, word):
+        current = self
+        for char in word:
+            if char in current.children:
+                current = current.children[char]
+            else:
+                current.children[char] = TrieNode()
+                current = current.children[char]
+        current.value += 1
+
+    def find(self, word):
+        current = self
+        for char in word:
+            if char in current.children:
+                current = current.children[char]
+            else:
+                return 0
+        return current.value
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        self.root.addWord(word)
+
+    def find(self, word):
+        return self.root.find(word)
+
+def wordsToTrie(text):
+    my_trie = Trie()
+    for line in text:
+        for word in line.split(" "):
+            word = word.strip("\n")
+            word = word.strip(".")
+            word = word.strip(",")
+            word = word.strip("(")
+            word = word.strip(")")
+            word = word.strip("\'")
+            word = word.strip("\"")
+            word = word.strip("!")
+            word = word.strip("?")
+            word = word.strip(":")
+            if word != "":
+                my_trie.addWord(word)
+    return my_trie.root
+
+def trieToTxt(trieNode, text = open("trieFreq.txt", "w"), stringToWrite = ""):
+    if trieNode.value > 0:
+        text.write(stringToWrite + " " + str(trieNode.value) + "\n")
+    if len(trieNode.children.keys()) > 0:
+        for key in trieNode.children:
+            trieToTxt(trieNode.children[key], text, stringToWrite + key)
+
+
+my_second_text = open("TextFile.txt", "r")
+my_trie_root = wordsToTrie(my_second_text)
+print(my_trie_root.children.keys())
+trieToTxt(my_trie_root)
+
+def freqDict(text):
+    my_dict = {}
+    for line in text:
+        word, freq = line.split(" ")
+        my_dict[word] = freq
+    return my_dict
+
+# dicttext = open("dictFreq.txt", "r")
+# dictFreq = freqDict(dicttext)
+trietext = open("trieFreq.txt", "r")
+trieFreq = freqDict(trietext)
+
+def check2dicts(dict1, dict2):
+    for key in dict1:
+        if dict1[key] != dict2[key]:
+            return False
+    for key in dict2:
+        if dict2[key] != dict1[key]:
+            return False
+    return True
+
+# print(dictFreq.keys())
+print(trieFreq.keys())
+# print(check2dicts(dictFreq, trieFreq))
---
+++ Week3/isBalancedTree.py
@@ -0,0 +1,208 @@
+from random import randrange
+
+class BSTNode:
+    def __init__(self, element, left, right):
+        self.element = element
+        self.left = left
+        self.right = right
+
+    def __repr__(self, nspaces=0):
+        s1 = ''
+        s2 = ''
+        s3 = ''
+        if self.right != None:
+            s1 = self.right.__repr__(nspaces + 3)
+        s2 = s2 + ' ' * nspaces + str(self.element) + '\n'
+        if self.left != None:
+            s3 = self.left.__repr__(nspaces + 3)
+        return s1 + s2 + s3
+
+    def insert(self, e):
+        parent = self
+        current = None
+        found = False
+
+        if parent.element < e:
+            current = parent.right
+        elif parent.element > e:
+            current = parent.left
+        else:
+            found = True;
+
+        while not found and current:
+            parent = current
+            if parent.element < e:
+                current = parent.right
+            elif parent.element > e:
+                current = parent.left
+            else:
+                found = True
+
+        if not found:
+            if parent.element < e:
+                parent.right = BSTNode(e, None, None)
+            else:
+                parent.left = BSTNode(e, None, None)
+        return not found
+
+    def insertArray(self, a, low=0, high=-1):
+        if len(a) == 0:
+            return
+        if high == -1:
+            high = len(a) - 1
+        mid = (low + high + 1) // 2
+        self.insert(a[mid])
+        if mid > low:
+            self.insertArray(a, low, mid - 1)
+        if high > mid:
+            self.insertArray(a, mid + 1, high)
+
+    def search(self, e):
+        current = self
+        found = False
+        while not found and current:
+            if current.element < e:
+                current = current.right
+            elif current.element > e:
+                current = current.left
+            else:
+                found = True
+        if found:
+            return current
+        else:
+            return None
+
+    def search2(self, e):
+        if self.element == e:
+            return self
+        parent = self.getParent(e)
+        if parent == None:
+            return None
+        if parent.element < e:
+            return parent.right
+        return parent.left
+
+    def getParent(self, e):
+        parent = self
+        current = None
+        found = False
+
+        if parent.element < e:
+            current = parent.right
+        elif parent.element > e:
+            current = parent.left;
+        else:
+            return None
+
+        while not found and current:
+            if current.element == e:
+                found = True
+            else:
+                parent = current
+                if current.element < e:
+                    current = current.right
+                else:
+                    current = current.left
+        if found:
+            return parent
+        else:
+            return None
+
+    def parentMinRightTree(self):
+        parent = self.right
+        current = parent.left
+        while current.left:
+            parent = current
+            current = current.left
+        return parent
+
+    def delete(self, e):
+        parent = self.getParent(e);
+
+        if parent == None:
+            return False
+        if parent.element < e:
+            current = parent.right
+            if current.left == None:
+                parent.right = parent.right.right
+                return True
+            else:
+                if current.right == None:
+                    parent.right = parent.right.left
+                    return True
+        else:
+            current = parent.left
+            if current.left == None:
+                parent.left = parent.left.right
+                return True
+            else:
+                if current.right == None:
+                    parent.left = parent.left.left
+                    return True
+        if current.right.left == None:
+            current.element = current.right.element
+            current.right = current.right.right
+            return True
+        node = current.parentMinRightTree()
+        current.element = node.left.element
+        node.left = node.left.right
+        return True
+
+
+class BST:
+    def __init__(self, a=None):
+        if a:
+            mid = len(a) // 2
+            self.root = BSTNode(a[mid], None, None)
+            self.root.insertArray(a[:mid])
+            self.root.insertArray(a[mid + 1:])
+        else:
+            self.root = None
+
+    def __repr__(self):
+        if self.root:
+            return str(self.root)
+        else:
+            return 'null-tree'
+
+    def search(self, e):
+        if self.root and e:
+            return self.root.search(e)
+        else:
+            return None
+
+    def insert(self, e):
+        if e:
+            if self.root:
+                return self.root.insert(e)
+            else:
+                self.root = BSTNode(e, None, None)
+                return True
+        else:
+            return False
+
+    def delete(self, e):
+        if self.root and e:
+            if self.root.element == e:
+                if self.root.left == None:
+                    self.root = self.root.right
+                elif self.root.right == None:
+                    self.root = self.root.left
+                elif self.root.right.left == None:
+                    self.root.element = self.root.right.element
+                    self.root.right = self.root.right.right
+                else:
+                    node = self.root.parentMinRightTree();
+                    self.root.element = node.left.element
+                    node.left = node.left.right
+                return True
+            else:
+                return self.root.delete(e)
+        else:
+            return False
+
+myList = []
+for number in range(1,100):
+    myList.append(randrange(1,100))
+myTree = BST(myList)
+print(myTree)
